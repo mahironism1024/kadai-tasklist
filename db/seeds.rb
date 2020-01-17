0707e012c1b5b53efb6b355acad71c6555786ed1
hiromu.nakajima <luck.hack.mahiro255@gmail.com>
2020-01-17 05:51:24 +0000

    views edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..100).each do |i|
+    Task.create(status: "test_status_#{i}", content: "test_content#{i}")
+end
